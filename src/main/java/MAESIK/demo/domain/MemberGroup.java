44e900d84922952bc9a557afd8c61a7fad7b6bc0
ubermen5che <ubermen5ch1308@gmail.com>
2022-04-08 12:17:19 +0900

    그룹내 각 유저들의 repoUrl 저장을 위한 필드추가

--- src/main/java/MAESIK/demo/domain/MemberGroup.java
+++ src/main/java/MAESIK/demo/domain/MemberGroup.java
@@ -1,9 +1,12 @@
 package MAESIK.demo.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -21,4 +24,7 @@ public class MemberGroup implements Serializable {
     @JoinColumn(name = "group_id")
     private Group group;
 
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_group_id")
+    private List<MemberGroupRepoUrl> memberGroupRepoUrlList = new ArrayList<>();
 }
