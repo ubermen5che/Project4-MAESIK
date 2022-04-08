25ecf15f788cf8049024cf9fbdc97053fb7f2546
ubermen5che <ubermen5ch1308@gmail.com>
2022-04-08 12:21:43 +0900

    [Service] 그룹 생성 서비스 로직 추가

--- src/main/java/MAESIK/demo/service/GroupService.java
+++ src/main/java/MAESIK/demo/service/GroupService.java
@@ -1,10 +1,14 @@
 package MAESIK.demo.service;
 
 import MAESIK.demo.domain.Group;
+import MAESIK.demo.domain.Member;
+import MAESIK.demo.domain.MemberGroup;
 import MAESIK.demo.repository.GroupRepository;
+import MAESIK.demo.repository.MemberGroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,10 +16,14 @@ import java.util.Optional;
 public class GroupService {
 
     private GroupRepository groupRepository;
+    private MemberGroupRepository memberGroupRepository;
+    private MemberService memberService;
 
     @Autowired
-    public GroupService(GroupRepository groupRepository) {
+    public GroupService(GroupRepository groupRepository, MemberGroupRepository memberGroupRepository, MemberService memberService) {
         this.groupRepository = groupRepository;
+        this.memberGroupRepository = memberGroupRepository;
+        this.memberService = memberService;
     }
 
     public Optional<Group> findGroup(Long id) {
@@ -24,5 +32,27 @@ public class GroupService {
 
     public List<Group> findAllGroup() {
         return groupRepository.findAll();
+    }
+
+    public Group createGroup(String groupName, String oAuthId) {
+        Group group = Group.builder()
+                .groupMasterId(oAuthId)
+                .groupName(groupName)
+                .build();
+
+        Group savedGroup = groupRepository.save(group);
+
+        MemberGroup memberGroup = MemberGroup.builder()
+                .group(savedGroup)
+                .memberGroupRepoUrlList(new ArrayList<>())
+                .build();
+
+        Optional<Member> member = memberService.findByOauthId(oAuthId);
+        member.get().getMemberGroupList().add(memberGroup);
+
+        memberGroupRepository.save(memberGroup);
+        memberService.save(member.get());
+
+        return savedGroup;
     }
 }
