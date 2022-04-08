2e3caf83a451409ae92e0df946f5101db186ff47
ubermen5che <ubermen5ch1308@gmail.com>
2022-04-08 12:19:46 +0900

    [Repository] 멤버가 가입한 모든 그룹의 정보를 꺼내오는 jpql 추가

--- src/main/java/MAESIK/demo/repository/GroupRepository.java
+++ src/main/java/MAESIK/demo/repository/GroupRepository.java
@@ -1,7 +1,11 @@
 package MAESIK.demo.repository;
 
 import MAESIK.demo.domain.Group;
+import MAESIK.demo.domain.Member;
+import MAESIK.demo.domain.dto.GroupResponseDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +14,12 @@ import java.util.Optional;
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
     Optional<Group> findById(Long id);
+    //Optional<Group> findByMasterId(Long id);
     List<Group> findAll();
+    @Query("SELECT new MAESIK.demo.domain.dto.GroupResponseDTO(g.id, g.groupName, g.groupExp, g.groupTier, g.groupMasterId)" +
+    " from Member m" +
+            " join m.memberGroupList mg" +
+            " join mg.group g" +
+            " where m.oauthId like %:memberId%")
+    List<GroupResponseDTO> findGroupDTOs(@Param("memberId") String memberId);
 }
