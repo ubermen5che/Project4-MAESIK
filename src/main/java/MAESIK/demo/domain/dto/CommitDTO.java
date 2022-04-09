09d94f3f5ad4bfa5fa8f09de75872b4f3f52763e
ubermen5che <ubermen5ch1308@gmail.com>
2022-04-10 00:01:16 +0900

    DTO에 존재하는 @Id 어노테이션 제거

--- src/main/java/MAESIK/demo/domain/dto/CommitDTO.java
+++ src/main/java/MAESIK/demo/domain/dto/CommitDTO.java
@@ -7,15 +7,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Id;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommitDTO {
 
-    @Id
     private String sha;
     private Commit commit;
     private String url;
