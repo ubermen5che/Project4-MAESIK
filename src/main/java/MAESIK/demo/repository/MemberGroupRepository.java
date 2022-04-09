c4f4ec1a6beb533a72ca56a8ba3e95ae44528dbc
ubermen5che <ubermen5ch1308@gmail.com>
2022-04-10 00:07:03 +0900

    Group search를 위한 method 추가

--- src/main/java/MAESIK/demo/repository/MemberGroupRepository.java
+++ src/main/java/MAESIK/demo/repository/MemberGroupRepository.java
@@ -3,8 +3,10 @@ package MAESIK.demo.repository;
 import MAESIK.demo.domain.MemberGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberGroupRepository extends JpaRepository<MemberGroup, Long> {
     Optional<MemberGroup> findById(Long id);
+    MemberGroup findByGroupId(Long groupId);
 }
