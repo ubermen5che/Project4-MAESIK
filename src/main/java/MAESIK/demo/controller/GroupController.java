1962a586551c68607060fb4ba0126fa40f0955a1
ubermen5che <ubermen5ch1308@gmail.com>
2022-04-09 23:59:37 +0900

    그룹 삭제, 그룹 repoUrl 업데이트를 위한 REST API 추가

--- src/main/java/MAESIK/demo/controller/GroupController.java
+++ src/main/java/MAESIK/demo/controller/GroupController.java
@@ -12,15 +12,18 @@ import MAESIK.demo.repository.MemberGroupRepository;
 import MAESIK.demo.service.GroupService;
 import MAESIK.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import javax.validation.Valid;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -61,7 +64,36 @@ public class GroupController {
         return ResponseEntity.created(location).body("{}");
     }
 
-    @PostMapping("/group/addRepoUrl")
+    @Transactional
+    @GetMapping("/group/delete/{id}")
+    public ResponseEntity<?> groupDelete(@PathVariable Long id, Authentication authentication) {
+        Member member = memberService.findByOauthId(authentication.getName()).get();
+
+        List<MemberGroup> memberGroupList = member.getMemberGroupList();
+
+        for (MemberGroup mg : memberGroupList) {
+            Group group = mg.getGroup();
+            if (group.getId() == id) {
+                if (group.getGroupMasterId() != member.getOauthId()) {
+                    return ResponseEntity.status(HttpStatus.FORBIDDEN).body("권한이 없습니다.");
+                }
+                groupRepository.deleteById(group.getId());
+                memberGroupRepository.delete(mg);
+                memberGroupList.remove(mg);
+                return ResponseEntity.ok().body(GroupResponseDTO.builder()
+                        .groupId(group.getId())
+                        .groupMasterId(group.getGroupMasterId())
+                        .groupExp(group.getGroupExp())
+                        .groupName(group.getGroupName())
+                        .groupTier(group.getGroupTier())
+                        .build());
+            }
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/group/add/repourl")
     public ResponseEntity<?> addRepoUrl(@Valid @RequestBody GroupRequestDTO groupRequestDTO, Authentication authentication) {
         Optional<Member> member = memberService.findByOauthId(authentication.getName());
 
@@ -89,5 +121,44 @@ public class GroupController {
         memberGroupRepository.save(searchedMemberGroup);
 
         return ResponseEntity.ok().body("{}");
+    }
+
+    @PostMapping("/group/update/repourl")
+    public ResponseEntity<?> updateRepoUrl(@Valid @RequestBody GroupRequestDTO groupRequestDTO, Authentication authentication) {
+        Optional<Member> member = memberService.findByOauthId(authentication.getName());
+
+        List<MemberGroup> memberGroupList = member.get().getMemberGroupList();
+
+        // 멤버가 가입한 그룹 리스트 둘 중 현재 url추가 하고자 하는 그룹을 필터링
+        MemberGroup searchedMemberGroup = null;
+
+        for (MemberGroup memberGroup : memberGroupList) {
+            if (memberGroup.getGroup().getId() == groupRequestDTO.getGroupId()) {
+                searchedMemberGroup = memberGroup;
+                break;
+            }
+        }
+
+        if (searchedMemberGroup == null) {
+            return ResponseEntity.badRequest().body("searchedMemberGroup is null");
+        }
+
+        List<MemberGroupRepoUrl> memberGroupRepoUrlList= searchedMemberGroup.getMemberGroupRepoUrlList();
+
+        Boolean isUpdated = false;
+
+        for (MemberGroupRepoUrl mu : memberGroupRepoUrlList) {
+            if (mu.getRepo().equals(groupRequestDTO.getRepoUrl())) {
+                mu.setRepo(groupRequestDTO.getUpdateUrl());
+                isUpdated = true;
+            }
+        }
+
+        if (isUpdated) {
+            memberGroupRepository.save(searchedMemberGroup);
+            return ResponseEntity.ok().body("{}");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
