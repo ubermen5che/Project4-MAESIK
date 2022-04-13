2bbd41c32fd6738088c314fcff0eb661a7eb0f16
ubermen5che <ubermen5ch1308@gmail.com>
2022-04-13 15:33:45 +0900

    유저 email 인증시 authentication없이 이메일 인증이 진행되도록 수정

--- src/main/java/MAESIK/demo/config/OAuth2SuccessHandler.java
+++ src/main/java/MAESIK/demo/config/OAuth2SuccessHandler.java
@@ -1,8 +1,10 @@
 package MAESIK.demo.config;
 
+import MAESIK.demo.domain.RefreshToken;
 import MAESIK.demo.domain.dto.TokenDto;
 import MAESIK.demo.domain.dto.UserDto;
 import MAESIK.demo.jwt.TokenService;
+import MAESIK.demo.repository.RefreshTokenRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,6 +26,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     private final TokenService tokenService;
     private final UserRequestMapper userRequestMapper;
     private final ObjectMapper objectMapper;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -31,7 +35,20 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
         //System.out.println("oAuthID : " + oAuth2User.getAttributes().get("id"));
         TokenDto tokenDto = tokenService.generateToken(authentication);
+
         log.info("{}", tokenDto);
+
+        Optional<RefreshToken> refreshToken = refreshTokenRepository.findByKey(authentication.getName());
+
+        if (refreshToken.isEmpty()) {
+            refreshTokenRepository.save(RefreshToken.builder()
+                    .key(authentication.getName())
+                    .value(tokenDto.getRefreshToken())
+                    .build());
+        } else {
+            refreshToken.get().updateValue(tokenDto.getRefreshToken());
+            refreshTokenRepository.save(refreshToken.get());
+        }
 
         writeTokenResponse(response, tokenDto);
     }
--- src/main/java/MAESIK/demo/config/SecurityConfig.java
+++ src/main/java/MAESIK/demo/config/SecurityConfig.java
@@ -7,37 +7,40 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 
 @EnableWebSecurity // spring security 설정을 활성화시켜주는 어노테이션
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    public SecurityConfig(OAuthService oAuthService, OAuth2SuccessHandler successHandler, JwtAuthFilter jwtAuthFilter, TokenService tokenService) {
+    public SecurityConfig(OAuthService oAuthService, OAuth2SuccessHandler successHandler, TokenService tokenService) {
         this.oAuthService = oAuthService;
         this.successHandler = successHandler;
-        this.jwtAuthFilter = jwtAuthFilter;
         this.tokenService = tokenService;
     }
 
     private final OAuthService oAuthService;
     private final OAuth2SuccessHandler successHandler;
-    private final JwtAuthFilter jwtAuthFilter;
     private final TokenService tokenService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .exceptionHandling()
+                .and()
+
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+
                 .authorizeRequests()
-                .antMatchers("/auth/**", "/index.html", "/login", "/oauth2").permitAll()
+                .antMatchers("/auth/**", "/index.html", "/login", "/oauth2", "/confirm-email").permitAll()
                 .anyRequest().authenticated()
                 .and()
+
                 .apply(new JwtSecurityConfig(tokenService))
                 .and()
+
                 .oauth2Login() // OAuth2 로그인 설정 시작점
                 .successHandler(successHandler)
                 .userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정 담당
