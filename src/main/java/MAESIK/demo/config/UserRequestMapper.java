a01d227f2a89022f217e779120af68cfc0437ad7
ubermen5che <ubermen5ch1308@gmail.com>
2022-05-22 17:38:30 +0900

    Edit property name (profile -> imageUrl)

--- src/main/java/MAESIK/demo/config/UserRequestMapper.java
+++ src/main/java/MAESIK/demo/config/UserRequestMapper.java
@@ -1,17 +1,17 @@
 package MAESIK.demo.config;
 
-import MAESIK.demo.domain.dto.UserDto;
+import MAESIK.demo.domain.dto.UserDTO;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserRequestMapper {
-    public UserDto toDto(OAuth2User oAuth2User) {
+    public UserDTO toDto(OAuth2User oAuth2User) {
         var attributes = oAuth2User.getAttributes();
-        return UserDto.builder()
+        return UserDTO.builder()
                 .oauthId((String)attributes.get("oauthId"))
                 .name((String)attributes.get("name"))
-                .picture((String)attributes.get("picture"))
+                .imageUrl((String)attributes.get("imageUrl"))
                 .build();
     }
 }
