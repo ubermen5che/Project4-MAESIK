30d707aa3eb9e0f608917af0a085370bf104e8c0
ubermen5che <ubermen5ch1308@gmail.com>
2022-05-22 21:59:27 +0900

    Add interface to find member by githubId

--- src/main/java/MAESIK/demo/repository/MemberRepository.java
+++ src/main/java/MAESIK/demo/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package MAESIK.demo.repository;
 
 import MAESIK.demo.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,7 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByOauthId(String id);
     Optional<Member> findById(Long id);
+    //@Query("select m from Member m join MemberGroup mg where m.memberId = mg.member")
     List<Member> findAll();
+    Optional<Member> findByGithubId(String id);
 }
