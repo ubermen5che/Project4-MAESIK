15bd745eef4191f27d5819ee2ff4e1ea344a8c82
ubermen5che <ubermen5ch1308@gmail.com>
2022-05-22 22:26:17 +0900

    [ADD] 토큰 검증 filter 추가

---
+++ src/main/java/MAESIK/demo/security/TokenAuthenticationFilter.java
@@ -0,0 +1,75 @@
+package MAESIK.demo.security;
+
+import MAESIK.demo.jwt.TokenService;
+import MAESIK.demo.service.MemberService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Slf4j
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    @Autowired
+    private TokenService tokenService;
+
+    private static final Logger logger = LoggerFactory.getLogger(TokenAuthenticationFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = resolveToken(request);
+
+            if (StringUtils.hasText(jwt) && tokenService.verifyToken(jwt)) {
+                UsernamePasswordAuthenticationToken authentication = tokenService.getAuthentication(jwt);
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (ExpiredJwtException | io.jsonwebtoken.security.SecurityException |
+                UnsupportedJwtException | IllegalArgumentException | MalformedJwtException e) {
+            log.error("Fail Decode Authorization Token");
+
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+            Map<String, Object> body = new LinkedHashMap<>();
+            body.put("code", HttpStatus.UNAUTHORIZED.value());
+            body.put("error", e.getMessage());
+
+            new ObjectMapper().writeValue(response.getOutputStream(), body);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
