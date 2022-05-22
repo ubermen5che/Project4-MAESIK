9e71d14029527fe38ad6c253e96ef3f58528c61c
ubermen5che <ubermen5ch1308@gmail.com>
2022-05-22 22:04:43 +0900

    Add group invitation acception code

--- src/main/java/MAESIK/demo/service/MemberService.java
+++ src/main/java/MAESIK/demo/service/MemberService.java
@@ -3,11 +3,14 @@ package MAESIK.demo.service;
 import MAESIK.demo.domain.ConfirmationToken;
 import MAESIK.demo.domain.Group;
 import MAESIK.demo.domain.Member;
+import MAESIK.demo.domain.MemberGroup;
+import MAESIK.demo.repository.GroupRepository;
+import MAESIK.demo.repository.MemberGroupRepository;
 import MAESIK.demo.repository.MemberRepository;
-import com.nimbusds.openid.connect.sdk.claims.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,11 +19,15 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final ConfirmationTokenService confirmationTokenService;
+    private final GroupRepository groupRepository;
+    private final MemberGroupRepository memberGroupRepository;
 
     @Autowired
-    public MemberService(MemberRepository memberRepository, ConfirmationTokenService confirmationTokenService) {
+    public MemberService(MemberRepository memberRepository, ConfirmationTokenService confirmationTokenService, GroupRepository groupRepository, MemberGroupRepository memberGroupRepository) {
         this.memberRepository = memberRepository;
         this.confirmationTokenService = confirmationTokenService;
+        this.groupRepository = groupRepository;
+        this.memberGroupRepository = memberGroupRepository;
     }
 
     public List<Member> findAllMember() {
@@ -37,6 +44,9 @@ public class MemberService {
         return memberRepository.findByOauthId(oauthId);
     }
 
+    public Optional<Member> findByGithubId(String githubId) {
+        return memberRepository.findByGithubId(githubId);
+    }
     /**
      * 이메일 인증 로직
      * @param token
@@ -50,6 +60,30 @@ public class MemberService {
         Optional<Member> member = findByOauthId(findConfirmationToken.getUserId());
         member.get().updateEmailAuth(true);
         findConfirmationToken.useToken();	// 토큰 만료 로직을 구현해주면 된다. ex) expired 값을 true로 변경
+        System.out.println("emailAuth : " + member.get().getEmailAuth());
         memberRepository.save(member.get());	// 유저의 이메일 인증 값 변경 로직을 구현해주면 된다. ex) emailVerified 값을 true로 변경
+    }
+
+    public Boolean inviteConfirm(String token, Long groupId) {
+        ConfirmationToken findConfirmationToken = confirmationTokenService.findByIdAndExpirationDateAfterAndExpired(token);
+
+        if (findConfirmationToken == null) // 토큰 만료 체크
+            return false;
+
+        Optional<Member> member = findByGithubId(findConfirmationToken.getUserId());
+        // Member의 MemberGroupList에
+        Optional<Group> group = groupRepository.findById(groupId);
+        MemberGroup memberGroup = MemberGroup.builder()
+                .group(group.get())
+                .memberGroupRepoUrlList(new ArrayList<>())
+                .build();
+
+        memberGroupRepository.save(memberGroup);
+        member.get().getMemberGroupList().add(memberGroup); // 초대받은 그룹에 멤버 추가
+
+        findConfirmationToken.useToken();
+        memberRepository.save(member.get());
+
+        return true;
     }
 }
