34b22fade2a6c15a683b045668e6c92d1aa3c042
ubermen5che <ubermen5ch1308@gmail.com>
2022-05-26 01:43:49 +0900

    [ADD] groupId를 기준으로 MemberGroup 찾아올 수 있는 메소드 추가

--- src/main/java/MAESIK/demo/repository/MemberGroupRepository.java
+++ src/main/java/MAESIK/demo/repository/MemberGroupRepository.java
@@ -1,12 +1,14 @@
 package MAESIK.demo.repository;
 
+import MAESIK.demo.domain.Member;
 import MAESIK.demo.domain.MemberGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberGroupRepository extends JpaRepository<MemberGroup, Long> {
     Optional<MemberGroup> findById(Long id);
-    MemberGroup findByGroupId(Long groupId);
+    List<MemberGroup> findByGroup_Id(Long groupId);
 }
