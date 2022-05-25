c20fdfb3848ec40a59aed32a94982986f1ad1489
ubermen5che <ubermen5ch1308@gmail.com>
2022-05-26 01:40:39 +0900

    [EDIT] MemberGroupRepoUrl Entitiy와 다대일 관계 설정

--- src/main/java/MAESIK/demo/domain/CommitDAO.java
+++ src/main/java/MAESIK/demo/domain/CommitDAO.java
@@ -5,9 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Date;
 import java.time.LocalDate;
 
@@ -20,12 +18,18 @@ import java.time.LocalDate;
 public class CommitDAO {
 
     @GeneratedValue @Id
+    @Column(name = "COMMIT_ID")
     private Long commitId;
     private String sha;
     private String url;
     private String name;
     private String email;
-    private Long date;
+    private Long commitDate;
     private String message;
     private String commitUrl;
+    private Long memberGroupRepoId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MemberGroupRepoUrl memberGroupRepoUrls;
+
 }
