1307f6ee0a2ec039691bf1c2eb24531ff1d424b6
ubermen5che <ubermen5ch1308@gmail.com>
2022-05-26 01:46:42 +0900

    [ADD] 통계 데이터 API 및 특정 시간 기준 모든 commit 데이터 읽기 위한 method추가

--- src/main/java/MAESIK/demo/repository/CommitRepository.java
+++ src/main/java/MAESIK/demo/repository/CommitRepository.java
@@ -1,12 +1,18 @@
 package MAESIK.demo.repository;
 
 import MAESIK.demo.domain.CommitDAO;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CommitRepository extends JpaRepository<CommitDAO, Long> {
-    Optional<CommitDAO> findDistinctTopByOrderByDateDesc();
+    Optional<CommitDAO> findDistinctTopByMemberGroupRepoIdOrderByCommitDateDesc(Long memberGroupRepoId);
+    List<CommitDAO> findByMemberGroupRepoId(Long memberGroupRepoId);
+    List<CommitDAO> findByMemberGroupRepoIdAndCommitDateGreaterThanOrderByCommitDateDesc(Long memberGroupRepoId, Long dateTime);
 }
